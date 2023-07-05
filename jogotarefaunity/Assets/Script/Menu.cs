ff37379ee80457f04538bc518f82897ede6405a4
Multiplo <mateussoprani1701@gmail.com>
2023-07-05 10:53:11 -0300

    Botão funcionando
    Fiz o botão FLAMENGOOOOO funcionar

---
+++ jogotarefaunity/Assets/Script/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string nomeCena;
+    public GameObject Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ativando painel do menu
+        Panel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //função do botão start
+    public void StartGame()
+    {
+        //carregando a cena Game
+        SceneManager.LoadScene(nomeCena);
+    }
+}
